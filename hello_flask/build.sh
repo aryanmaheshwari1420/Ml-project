f72b7092458b9a5cfb1ded9a261b1b8bab594175
Aryan Maheshwari <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-02-02 16:41:00 +0530

    Update build.sh

--- hello_flask/build.sh
+++ hello_flask/build.sh
@@ -1,9 +1,7 @@
 python3 --version 
-wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-aarch64.sh
-bash Anaconda3-2022.10-Linux-aarch64.sh
-conda activate base
-pip install flask
-pip install sklearn
-pip install pandas 
-pip install numpy
-python3 app.py
+python3 -m pip -V
+# pip install flask
+# pip install sklearn
+# pip install pandas 
+# pip install numpy
+# python3 app.py
